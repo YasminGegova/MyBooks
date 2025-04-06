afff0715afe3b30a80a59a1dc4778455be7eeda1
YasminGegova <qsmin41@gmail.com>
2025-04-06 15:04:26 +0300

    Add Book dialog

--- app/src/main/java/com/example/mybooks/BookDetailsActivity.kt
+++ app/src/main/java/com/example/mybooks/BookDetailsActivity.kt
@@ -1,21 +1,24 @@
 package com.example.mybooks
 
-import android.content.Intent
+import android.app.Dialog
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class BookDetailsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_details)
         val btnQuotes: Button = findViewById(R.id.btnQuotes)
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.book_dialog)
         btnQuotes.setOnClickListener {
-            val intent = Intent(this, QuotesActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(this, QuotesActivity::class.java)
+//            startActivity(intent)
+
+            dialog.show()
+
         }
     }
 }
