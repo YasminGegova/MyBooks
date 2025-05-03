7d3eeabe1f3e5619c6a392192519652a57c03667
YasminGegova <qsmin41@gmail.com>
2025-05-03 15:06:24 +0300

    Implement localization for english and bulgarian support

--- app/src/main/java/com/example/mybooks/ui/MainActivity.kt
+++ app/src/main/java/com/example/mybooks/ui/MainActivity.kt
@@ -1,17 +1,43 @@
 package com.example.mybooks.ui
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mybooks.R
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private val PREF_LANGUAGE_KEY = "language"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var activityType : String
+
+        // Initialize SharedPreferences
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val storedLanguage = getLanguage()
+        updateLocale(storedLanguage)
+        if (savedInstanceState == null) {
+            recreate()
+        }
+
+        val sLanguage : Switch = findViewById(R.id.sLanguage)
+        sLanguage.isChecked = storedLanguage == "bg"
+
+        sLanguage.setOnClickListener {
+            val language = if (sLanguage.isChecked) "bg" else "en"
+            setLanguage(language)
+            updateLocale(language)
+            recreate()
+        }
 
         // Configure click listeners for all books button
         val btnAllBooks : Button = findViewById(R.id.btnAllBooks)
@@ -38,4 +64,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun setLanguage(language: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString(PREF_LANGUAGE_KEY, language)
+        editor.apply()
+    }
+
+    private fun getLanguage(): String {
+        return sharedPreferences.getString(PREF_LANGUAGE_KEY, null) ?: "en"
+    }
+
+    private fun updateLocale(language: String) {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val resources: Resources = this.resources
+        val configuration: Configuration = resources.configuration
+        configuration.setLocale(locale)
+        resources.updateConfiguration(configuration, resources.displayMetrics)
+    }
+
 }
