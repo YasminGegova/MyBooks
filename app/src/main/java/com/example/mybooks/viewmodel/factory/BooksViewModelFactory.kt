f1802e1ff355cb927bd29b6038009d988ce8a0df
YasminGegova <qsmin41@gmail.com>
2025-06-06 16:42:28 +0300

    Update the activity type and resources handling

--- app/src/main/java/com/example/mybooks/viewmodel/factory/BooksViewModelFactory.kt
+++ app/src/main/java/com/example/mybooks/viewmodel/factory/BooksViewModelFactory.kt
@@ -3,16 +3,17 @@ package com.example.mybooks.viewmodel.factory
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.mybooks.ui.BooksActivity
 import com.example.mybooks.viewmodel.BooksViewModel
 
 @Suppress("UNCHECKED_CAST")
 class BooksViewModelFactory(
-    private val application: Application,
+    private val activity: BooksActivity,
     private val activityType: String
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(BooksViewModel::class.java)) {
-            return BooksViewModel(application, activityType) as T
+            return BooksViewModel(activity, activityType) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
