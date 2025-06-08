3a75b6b0dcd970670b299219faef44bc689f4f0e
YasminGegova <qsmin41@gmail.com>
2025-06-08 23:20:10 +0300

    Fix ViewModel activity dependency.

--- app/src/main/java/com/example/mybooks/viewmodel/factory/BooksViewModelFactory.kt
+++ app/src/main/java/com/example/mybooks/viewmodel/factory/BooksViewModelFactory.kt
@@ -1,6 +1,7 @@
 package com.example.mybooks.viewmodel.factory
 
 import android.app.Application
+import android.content.res.Resources
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.mybooks.ui.BooksActivity
@@ -8,12 +9,13 @@ import com.example.mybooks.viewmodel.BooksViewModel
 
 @Suppress("UNCHECKED_CAST")
 class BooksViewModelFactory(
-    private val activity: BooksActivity,
+    private val application: Application,
+    private val resources: Resources,
     private val activityType: String
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(BooksViewModel::class.java)) {
-            return BooksViewModel(activity, activityType) as T
+            return BooksViewModel(application, resources, activityType) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
